d9ef2e750781a9878ec8f3a3e88513c246b0d2b6
Lorenzo Martinez <lmartinezg_hm@hotmail.com>
2016-11-01 08:20:32 +0100

    Solved problem with session variables. Incorrect "==" instead of "=" when setting $_SESSION[autentificado] in control.php

--- bextlan-php-Tutorial_MVC/WebContent/05-Sesiones/control.php
+++ bextlan-php-Tutorial_MVC/WebContent/05-Sesiones/control.php
@@ -1,10 +1,9 @@
 <?php
-if ($_POST ["usuario_txt"] == "bextlan" && $_POST ["password_txt"] == "1234") {
-	// Inicio la sesión
-	session_start ();
+session_start ();
+if ($_POST ["usuario_txt"] == "lmg" && $_POST ["password_txt"] == "1234") {
 	
 	// Declarar variables de sesión
-	$_SESSION ["autentificado"] == true;
+	$_SESSION ["autentificado"] = true;
 	$_SESSION ["usuario"] = $_POST ["usuario_txt"];
 	
 	header ( "Location: archivo-protegido.php" );
