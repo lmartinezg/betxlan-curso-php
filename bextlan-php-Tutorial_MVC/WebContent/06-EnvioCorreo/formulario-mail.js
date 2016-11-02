64145402ed9e2d6e0bb035ae73ec3f87168f2978
luispuchades <luispuchades@live.com>
2016-11-02 23:29:12 +0100

    Corregido html añadiendo id a los inputs y corregido js con asignación de variables a los id

--- bextlan-php-Tutorial_MVC/WebContent/06-EnvioCorreo/formulario-mail.js
+++ bextlan-php-Tutorial_MVC/WebContent/06-EnvioCorreo/formulario-mail.js
@@ -1,42 +1,69 @@
 // Javascript document
 
+
+//1. DECLARACION DE OBJETOS Y VARIABLES
+var mailFrm;
+var deTxt;
+var paraTxt;
+var asuntoTxt;
+var mensajeTxa;
+var enviarBtn;
+
+//1.1 ASIGNACION DE VARIABLES
+mailFrm = document.getElementByID("mail-frm");
+deTxt = document.getElementByID("de-txt");
+paraTxt = document.getElementByID("para-txt");
+asuntoTxt = document.getElementByID("asunto-txt");
+mensajeTxa = document.getElementByID("mensaje-txa");
+enviarBtn = document.getElementByID("enviar-btn");
+
+
+
+//2. DECLARACION DE FUNCIONES
 function validarForm() {
 
-	var verificar = true;
-	var expRegEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    var verificar = true;
+    var expRegEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
-	if (!document.mail_frm.de_txt.value) {
-		alert("El campo 'De' es requerido.");
-		document.mail_frm.de_txt.focus();
-		verificar = false;
-	} else if (!expRegEmail.exec(document.mail_frm.de_txt.value)) {
-		alert("La dirección de correo 'De' no es válida.");
-		document.mail_frm.de_txt.focus();
-		verificar = false;
-	} else if (!document.mail_frm.para_txt.value) {
-		alert("El campo 'Para' es requerido.");
-		document.mail_frm.para_txt.focus();
-		verificar = false;
-	} else if (!expRegEmail.exec(document.mail_frm.para_txt.value)) {
-		alert("La dirección de correo 'Para' no es válida.");
-		document.mail_frm.para_txt.focus();
-		verificar = false;
-	} else if (!document.mail_frm.asunto_txt.value) {
-		alert("El campo 'Asunto' es requerido.");
-		document.mail_frm.asunto_txt.focus();
-		verificar = false;
-	} else if (!document.mail_frm.mensaje_txa.value) {
-		alert("El campo 'Mensaje' es requerido.");
-		document.mail_frm.mensaje_txa.focus();
-		verificar = false;
-	}
+    if (!mailFrm.value) {
+        alert("El campo 'De' es requerido.");
+        mailFrm.focus();
+        verificar = false;
+    } else if (!expRegEmail.exec(deTxt.value)) {
+        alert("La dirección de correo 'De' no es válida.");
+        deTxt.focus();
+        verificar = false;
+    } else if (!paraTxt.value) {
+        alert("El campo 'Para' es requerido.");
+        paraTxt.focus();
+        verificar = false;
+    } else if (!expRegEmail.exec(paraTxt.value)) {
+        alert("La dirección de correo 'Para' no es válida.");
+        paraTxt.focus();
+        verificar = false;
+    } else if (!asuntoTxt.value) {
+        alert("El campo 'Asunto' es requerido.");
+        asuntoTxt.focus();
+        verificar = false;
+    } else if (!mensajeTxa.value) {
+        alert("El campo 'Mensaje' es requerido.");
+        mensajeTxa.focus();
+        verificar = false;
+    }
 
-	if (verificar) {
-		document.mail_frm.submit();
-	}
+    if (verificar) {
+        mailFrm.submit();
+    }
 
 }
 
-window.onload = function() {
-	document.mail_frm.enviar_btn.onclick = validarForm;
+
+function alCargarDocumento() {
+    "use strict";
+
+    enviarBtn.addEventListener("click", validarForm);
 }
+
+
+//3. ASIGNACION DE EVENTOS
+window.addEventListener("load", alCargarDocumento);
