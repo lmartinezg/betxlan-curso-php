d3ba40879b13e6efbae70ed73c67d33cbfc5d25d
Lorenzo Martinez <lmartinezg_hm@hotmail.com>
2016-11-03 23:51:54 +0100

    Prepare mail headers

--- bextlan-php-Tutorial_MVC/WebContent/06-EnvioCorreo/enviar-mail.php
+++ bextlan-php-Tutorial_MVC/WebContent/06-EnvioCorreo/enviar-mail.php
@@ -1,1 +1,17 @@
 <?php
+$de = $_POST ["de_txt"];
+$para = $_POST ["para_txt"];
+$asunto = $_POST ["asunto_txt"];
+$mensaje = $_POST ["mensaje_txa"];
+
+$cabeceras = "MIME-Version: 1-0\r\n";
+$cabeceras .= "Content-Type: text/html; charset=iso-8859-1\r\n";
+$cabeceras .= "From: $de \r\n";
+
+if (mail ( $para, $asunto, $mensaje, $cabeceras )) {
+	$respuesta = "El mensaje ha sido enviado.";
+} else {
+	$respuesta = "Ocurrió un error. No se envió el mensaje.";
+}
+header ( "Location: formulario-mail.php?respuesta=$respuesta" );
+?>
